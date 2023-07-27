53a2d77373535ec853c5273fac38bb65f9429d62
leiweichang <leiweichang@funo.com.cn>
2023-07-27 17:11:01 +0800

    添加读取execl代码

---
+++ src/main/java/com/testspringboot/entity/ExeclEntity.java
@@ -0,0 +1,17 @@
+package com.testspringboot.entity;
+
+import lombok.Data;
+
+/**
+ * @program: testSpringboot
+ * @ClassName ExeclEntity
+ * @description:
+ * @author: 雷维昌
+ * @create: 2023-07-27 15:36
+ * @Version 1.0
+ **/
+@Data
+public class ExeclEntity {
+    private String rowOne;
+    private String rowTwo;
+}
---
+++ src/test/java/com/testspringboot/utils/ExcelReadrUtilTest.java
@@ -0,0 +1,39 @@
+package com.testspringboot.utils;
+import com.testspringboot.entity.ExeclEntity;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @program: testSpringboot
+ * @ClassName ExcelReadrUtilTest
+ * @description:
+ * @author: 雷维昌
+ * @create: 2023-07-27 15:37
+ * @Version 1.0
+ **/
+public class ExcelReadrUtilTest {
+
+    @Test
+    public void test()  {
+        File file = new File("E:\\FUNO_leiweichang\\oneGrad.xlsx");
+        List<ExeclEntity> excelUserEntities = null;
+        try {
+            excelUserEntities = ExcelReaderUtil.readExcel2Bean(new FileInputStream(file), ExeclEntity.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+        for (ExeclEntity excelUserEntity : excelUserEntities) {
+            System.out.println("update Priority set name ='"+excelUserEntity.getRowTwo()+"' where name='"+excelUserEntity.getRowOne()+"';");
+        }
+
+    }
+}
+
